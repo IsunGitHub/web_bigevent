cc206928fc2f234cdf6f2691f20b5b01a70036df
Isun <1098017823@qq.com>
2021-02-01 08:28:18 +0800

    完成登录功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -45,4 +45,22 @@ $(function(){
     })
   })
 
+  //登录功能
+  $("#form_login").on("submit",function(e){
+    e.preventDefault()
+    $.ajax({
+      method:"POST",
+      url:"http://ajax.frontend.itheima.net/api/login",
+      data:$(this).serialize(),
+      success(res) {
+        if(res.status !== 0) {
+          return layer.msg(res.message)
+        }
+        layer.msg("登录成功")
+        localStorage.setItem("token",res.token)
+        location.href = "/index.html"    
+      }
+    })
+  })
+
 })
